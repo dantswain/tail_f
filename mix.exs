df72078e17e4d3c2270264713d1249ae8b22cc51
Dan Swain <dan.t.swain@gmail.com>
2014-11-29 16:55:07 -0600

    Make dialyze a dev-only dep

--- mix.exs
+++ mix.exs
@@ -25,6 +25,6 @@ defmodule TailF.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:dialyze, "~> 0.1.3"}]
+    [{:dialyze, "~> 0.1.3", only: :dev}]
   end
 end
